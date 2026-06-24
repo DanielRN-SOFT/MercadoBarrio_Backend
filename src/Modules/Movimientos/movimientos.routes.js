f72ac5a9945d183ad1e827a6c3eef532d5924ae8
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-24 16:39:08 -0500

    DateNow corregido, y ruta de eliminar movimiento

--- src/Modules/Movimientos/movimientos.routes.js
+++ src/Modules/Movimientos/movimientos.routes.js
@@ -3,6 +3,7 @@ import {
   getMovements,
   getMovementById,
   createMovements,
+  cancelMovement,
 } from "./movimientos.controller.js";
 import {
   attachStore,
@@ -13,6 +14,7 @@ import {
 const router = express.Router();
 router.get("/", protect, isGrocer, attachStore, getMovements);
 router.get("/:id", protect, isGrocer, attachStore, getMovementById);
-router.get("/", protect, isGrocer, attachStore, createMovements);
+router.post("/", protect, isGrocer, attachStore, createMovements);
+router.post("/delete/:id", protect, isGrocer, attachStore, cancelMovement);
 
 export default router;
--- src/helpers/getDateNow.js
+++ src/helpers/getDateNow.js
@@ -1,16 +1,3 @@
 export default function getDateNow() {
-  const now = new Date();
-
-  // Obtener componentes de la fecha
-  const año = now.getFullYear();
-  const mes = String(now.getMonth() + 1).padStart(2, "0"); // Los meses van de 0 a 11
-  const dia = String(now.getDate()).padStart(2, "0");
-
-  // Obtener componentes de la hora
-  const horas = String(now.getHours()).padStart(2, "0");
-  const minutos = String(now.getMinutes()).padStart(2, "0");
-  const segundos = String(now.getSeconds()).padStart(2, "0");
-
-  // Unir todo en el formato de MySQL
-  return `${año}-${mes}-${dia} ${horas}:${minutos}:${segundos}`;
+  return new Date();
 }
